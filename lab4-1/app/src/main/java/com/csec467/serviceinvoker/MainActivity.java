55549863da0e8fa815170fe831aa72e92d307e89
caj2468 <caj2468@rit.edu>
2025-02-26 23:16:44 -0500

    idek

--- lab4-1/app/src/main/java/com/csec467/serviceinvoker/MainActivity.java
+++ lab4-1/app/src/main/java/com/csec467/serviceinvoker/MainActivity.java
@@ -1,24 +1,59 @@
 package com.csec467.serviceinvoker;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "ServiceInvoker";
+    private static final String SSID_BROADCAST = "com.csec467.SSID_BROADCAST";
+
+    private final BroadcastReceiver ssidReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction() != null && intent.getAction().equals(SSID_BROADCAST)) {
+                String ssid = intent.getStringExtra("SSID");
+                Log.d(TAG, "Received SSID: " + ssid);
+                Toast.makeText(context, "Connected SSID: " + ssid, Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button invokeButton = findViewById(R.id.invokeButton);
+        invokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClassName("com.csec467.servicecreator", "com.csec467.servicecreator.MyService");
+                startService(intent);
+                Log.d(TAG, "Service invoked");
+            }
         });
+
+        // Register Broadcast Receiver
+        registerReceiver(ssidReceiver, new IntentFilter(SSID_BROADCAST), Context.RECEIVER_EXPORTED);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(ssidReceiver);
     }
 }
--- lab4/app/src/main/java/com/csec467/servicecreator/MyService.java
+++ lab4/app/src/main/java/com/csec467/servicecreator/MyService.java
@@ -1,25 +1,41 @@
 package com.csec467.servicecreator;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
+
+    private static final String TAG = "MyService";
+    public static final String SSID_BROADCAST = "com.csec467.SSID_BROADCAST";
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        String ssid = wifiManager.getConnectionInfo().getSSID();
-        // Broadcast the SSID
-        Intent broadcastIntent = new Intent("com.example.SSID_BROADCAST");
-        broadcastIntent.putExtra("SSID", ssid);
-        sendBroadcast(broadcastIntent);
+        new Thread(this::sendSSIDBroadcast).start();
         return START_NOT_STICKY;
+    }
+
+    private void sendSSIDBroadcast() {
+        try {
+            WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            String ssid = wifiInfo.getSSID();
+
+            Intent broadcastIntent = new Intent(SSID_BROADCAST);
+            broadcastIntent.putExtra("SSID", ssid);
+            sendBroadcast(broadcastIntent);
+            Log.d(TAG, "Broadcast sent with SSID: " + ssid);
+        } catch (Exception e) {
+            Log.e(TAG, "Error getting SSID", e);
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return new Binder();
     }
 }
